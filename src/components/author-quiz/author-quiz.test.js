8d9d5b049dbf9f33c9e5f436a0f928761ab3dbb6
Cochran, Ryan J <ryan.j.cochran@monsanto.com>
2019-04-02 12:35:11 -0500

    should work

--- src/components/author-quiz/author-quiz.test.js
+++ src/components/author-quiz/author-quiz.test.js
@@ -31,14 +31,14 @@ afterAll(() => {
 });
 
 // Basic Constructs
-describe.skip("Test Filters", () => {
+describe("Test Filters", () => {
 
   beforeEach(() => {
     console.log('each');
   });
 
   it("Test 1", () => {
-    expect(1).toBe(2);
+    expect(1).toBe(1);
   });
   it("Test 2", () => {
     expect(2).toBe(2);
